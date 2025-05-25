4fbc468bb0be9475dad3bad6de1302a55dbd1cdb
Meow-nah-ngo <thitaree1811@gmail.com>
2025-05-25 19:43:07 +0700

    2

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,9 +1,19 @@
+import Navbar from './Navbar.jsx'
+
 function App() {
   return (
-  <div>
-    <h1 className="head-text">React webapp</h1>
-  </div>
+    <div>
+      <h1>zaan</h1>
+      <img src="" alt="" />
+      <h2>Quote</h2>
+      <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, dicta. Eos reiciendis cum aut recusandae, dicta magnam dignissimos ratione aspernatur facere sunt suscipit temporibus. Optio deleniti magnam quasi quia nesciunt.</h3>
+      <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore earum tempora nulla atque repellat! Error quam vitae accusamus dolorum, esse illo autem fugit vel ex iure voluptate minima, omnis architecto.</h3>
+      <h4 className='poem-syntax'>"</h4>
+      <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio assumenda illum quis sed animi dolores exercitationem voluptatem quibusdam iusto omnis, quod neque repellat totam molestias, deserunt atque architecto. Ullam, in!</h4>
+      <h5>Name -CEO</h5>
+    </div>
   )
+
 }
 
 export default App
---
+++ react/src/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar() {
+    return(
+    <nav className="navbar">
+        <a href="/" className="site-title">zaan</a>
+        <ul>
+            <li className="active"><a href="/about">about</a></li>
+            <li><a href="/profile">profile</a></li>
+            <li><a href="/product">product</a></li>
+            <li><a href="/solution">solution</a></li>
+            <li><a href="/contact us">contact us</a></li>
+        </ul>
+    </nav>
+    )
+}
+
+export default Navbar
