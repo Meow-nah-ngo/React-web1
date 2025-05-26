a4b348f5f86f6197ddcae476c1a5eb02b7cf62b4
Meow-nah-ngo <thitaree1811@gmail.com>
2025-05-26 11:00:31 +0700

    4

--- react/src/Footer.jsx
+++ react/src/Footer.jsx
@@ -1,10 +1,18 @@
 function Footer() {
-    return(
-        <footer>
-            <h4 className="contact">contact us</h4>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse pariatur corrupti dolor, quam vero tenetur atque odit neque asperiores. Sequi quod voluptas nostrum alias quidem ullam reprehenderit eum esse animi.</p>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio saepe sit cupiditate quos, consequatur nisi et quam assumenda voluptatum repellat, distinctio blanditiis, modi obcaecati nesciunt quibusdam praesentium facere voluptate aperiam?</p>
-        </footer>
-    )
+  return (
+    <footer className="footer">
+      <h4>Contact Us</h4>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt, iste aspernatur
+        temporibus rem cupiditate veniam laborum unde praesentium odit, recusandae id
+        molestias impedit voluptas sit amet delectus eum dolorem fuga.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus soluta quis
+        voluptas, suscipit iste consectetur minima esse praesentium, quisquam facere
+        molestiae odit, exercitationem sequi. Natus pariatur voluptas in ab delectus!
+      </p>
+    </footer>
+  )
 }
 export default Footer
--- react/src/Navbar.jsx
+++ react/src/Navbar.jsx
@@ -3,7 +3,7 @@ function Navbar() {
     <nav className="navbar">
         <a href="/" className="site-title">zaan</a>
         <ul>
-            <li className="active"><a href="/about">about</a></li>
+            <li><a href="/about">about</a></li>
             <li><a href="/profile">profile</a></li>
             <li><a href="/product">product</a></li>
             <li><a href="/solution">solution</a></li>
