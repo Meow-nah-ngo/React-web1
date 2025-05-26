57674bbe8496bd5c2ad6ce194722e2f4fd7d5a0c
Meow-nah-ngo <thitaree1811@gmail.com>
2025-05-26 09:36:27 +0700

    4

---
+++ react/src/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return(
+        <footer>
+            <h4 className="contact">contact us</h4>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse pariatur corrupti dolor, quam vero tenetur atque odit neque asperiores. Sequi quod voluptas nostrum alias quidem ullam reprehenderit eum esse animi.</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio saepe sit cupiditate quos, consequatur nisi et quam assumenda voluptatum repellat, distinctio blanditiis, modi obcaecati nesciunt quibusdam praesentium facere voluptate aperiam?</p>
+        </footer>
+    )
+}
+export default Footer
